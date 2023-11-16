adc5fbee4c46cc2c636ba1c0c2d7676308df55fc
ohhyeon2 <dhgus1235@gmail.com>
2023-11-16 19:13:04 +0900

    refactor:

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            OutputView.inputNumber();
+            final Set<Integer> input = InputView.input();
+            final RandomGenerator randomGenerator = new RandomGenerator();
+
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
     }
 }
--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -7,34 +7,34 @@ import java.util.Set;
 public class InputView {
 
     public static Set<Integer> input() {
-        String input = Console.readLine();
-        Set<Integer> nums = inputParsing(input);
+        final String input = Console.readLine();
+        final Set<Integer> nums = inputParsing(input);
         validateInput(input, nums);
         return nums;
     }
 
-    public static Set<Integer> inputParsing(String input) {
-        Set<Integer> nums = new HashSet<>();
-        for (String num : input.split("")) {
+    private static Set<Integer> inputParsing(final String input) {
+        final Set<Integer> nums = new HashSet<>();
+        for (final String num : input.split("")) {
             nums.add(validateNumber(num));
         }
         return nums;
     }
 
-    public static int validateNumber(String num) {
+    private static int validateNumber(final String num) {
         try {
             return Integer.parseInt(num);
         } catch (NumberFormatException e) {
-            throw new NumberFormatException("error : 숫자 외에 다른 문자의 입력은 안된다.");
+            throw new NumberFormatException("error : 세자리의 숫자만 입력해주세요.");
         }
     }
 
-    public static void validateInput(String input, Set<Integer> nums) {
+    private static void validateInput(final String input, final Set<Integer> nums) {
         if (input.isEmpty()) {
-            throw new IllegalArgumentException("error : 세자리의 숫자를 입력해주세요.");
+            throw new IllegalArgumentException("error : 세자리의 숫자만 입력해주세요.");
         }
         if (input.length() != 3) {
-            throw new IllegalArgumentException("error : 세자리의 숫자를 입력해주세요.");
+            throw new IllegalArgumentException("error : 세자리의 숫자만 입력해주세요.");
         }
         if (nums.size() != input.length()) {
             throw new IllegalArgumentException("error : 중복된 수는 입력할 수 없습니다.");
