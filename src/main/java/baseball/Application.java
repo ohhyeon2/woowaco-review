0e2c58b872379b29f7690950d3265521402214c7
ohhyeon2 <dhgus1235@gmail.com>
2023-11-16 19:14:23 +0900

    refactor: final 적용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,6 @@ import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         try {
             OutputView.inputNumber();
             final Set<Integer> input = InputView.input();
